3d717276980645f3448e37a021c1264a28089068
Sujay Shenoy <sujay.ss81@gmail.com>
2019-06-22 22:13:54 +0530

    Added designs to marks page

--- resources/views/insert_marks.blade.php
+++ resources/views/insert_marks.blade.php
@@ -2,23 +2,46 @@
 <html>
 <head>
 	<title>Insert Marks</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/bootstrap.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/marks_style.css')}}">
 </head>
 <body>
-		<form method="post" action="/marks">
-			<input type="hidden" name="_token" value="{{csrf_token()}}">
-			<label>Subject 1</label>
-			<input type="text" name="mark1">
-			<label>Subject 2</label>
-			<input type="text" name="mark2">
-			<label>Subject 3</label>
-			<input type="text" name="mark3">
-			<label>Subject 4</label>
-			<input type="text" name="mark4">
-			<label>Subject 5</label>
-			<input type="text" name="mark5">
-			<label>Subject 6</label>
-			<input type="text" name="mark6">
-			<input type="submit" name="submit" value="submit">
-		</form>	
+		<div class="container">
+			<form method="post" action="/marks">
+				<input type="hidden" name="_token" value="{{csrf_token()}}">
+				<div class="mycontainer">
+					<div class="form-group">
+						<div class="form-row">
+							<label>Subject 1</label>
+							<input type="text" name="mark1">
+						</div>	
+						<div class="form-row">
+							<label>Subject 2</label>
+							<input type="text" name="mark2">
+						</div>	
+						<div class="form-row">
+							<label>Subject 3</label>
+							<input type="text" name="mark3">
+						</div>	
+						<div class="form-row">
+							<label>Subject 4</label>
+							<input type="text" name="mark4">
+						</div>	
+						<div class="form-row">
+							<label>Subject 5</label>
+							<input type="text" name="mark5">
+						</div>	
+						<div class="form-row">
+							<label>Subject 6</label>
+							<input type="text" name="mark6">
+						</div>
+					</div>	
+				<div>
+					<input type="submit" class="btn btn-primary" name="submit" value="submit">
+				</div>
+			</form>	
+				<a class="btn btn-primary" id="a" onclick="window.location.href = '/choose_student'">Cancel</a>			
+		</div>	
+
 </body>
 </html>
--- resources/views/update_marks.blade.php
+++ resources/views/update_marks.blade.php
@@ -2,25 +2,48 @@
 <html>
 <head>
 	<title>Insert Marks</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/bootstrap.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/marks_style.css')}}">
 </head>
 <body>
-		<form method="post" action="/mupdate">
-			<input type="hidden" name="_token" value="{{csrf_token()}}">
-			@foreach($res as $item)
-			<label>Subject 1</label>
-			<input type="text" name="mark1" value="{{$item->mark1}}">
-			<label>Subject 2</label>
-			<input type="text" name="mark2" value="{{$item->mark2}}">
-			<label>Subject 3</label>
-			<input type="text" name="mark3" value="{{$item->mark3}}">
-			<label>Subject 4</label>
-			<input type="text" name="mark4" value="{{$item->mark4}}">
-			<label>Subject 5</label>
-			<input type="text" name="mark5" value="{{$item->mark5}}">
-			<label>Subject 6</label>
-			<input type="text" name="mark6" value="{{$item->mark6}}">
-			@endforeach
-			<input type="submit" name="submit" value="submit">
-		</form>	
+		<div class="container">
+			<form method="post" action="/mupdate">
+				<input type="hidden" name="_token" value="{{csrf_token()}}">
+				@foreach($res as $item)
+				<div class="mycontainer">
+					<div class="form-group">
+						<div class="form-row">
+							<label>Subject 1</label>
+							<input type="text" name="mark1" value="{{$item->mark1}}">
+						</div>	
+						<div class="form-row">
+							<label>Subject 2</label>
+							<input type="text" name="mark2" value="{{$item->mark2}}">
+						</div>	
+						<div class="form-row">
+							<label>Subject 3</label>
+							<input type="text" name="mark3" value="{{$item->mark3}}">
+						</div>	
+						<div class="form-row">
+							<label>Subject 4</label>
+							<input type="text" name="mark4" value="{{$item->mark4}}">
+						</div>	
+						<div class="form-row">
+							<label>Subject 5</label>
+							<input type="text" name="mark5" value="{{$item->mark5}}">
+						</div>	
+						<div class="form-row">
+							<label>Subject 6</label>
+							<input type="text" name="mark6" value="{{$item->mark6}}">
+						</div>
+					</div>	
+				<div>
+					<input type="submit" class="btn btn-primary" name="submit" value="submit">
+				</div>
+			</form>	
+				<a class="btn btn-primary" id="a" onclick="window.location.href = '/marks_details/{{$item->usn}}'">Cancel</a>			
+		</div>	
+				@endforeach
+
 </body>
 </html>
